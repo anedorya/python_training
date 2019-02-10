0e148ff556c865beaf417499704eaa745f508091
Anna Nedorya <a.nedorya@yandex.ru>
2019-02-10 16:01:21 +0300

    code improving2

--- test_add_group.py
+++ test_add_group.py
@@ -6,8 +6,10 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
+from group import Group
 
-class TestAddGroup(unittest.TestCase):
+
+class test_add_group(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
@@ -26,18 +28,18 @@ class TestAddGroup(unittest.TestCase):
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def add_group(self, wd, groupname, header, footer):
+    def add_group(self, wd, group):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(groupname)
+        wd.find_element_by_name("group_name").send_keys(group.groupname)
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("%s" % footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
@@ -47,21 +49,21 @@ class TestAddGroup(unittest.TestCase):
     def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
 
-    def add_group(self):
+    def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.add_group(wd, groupname="Group1", header="asdasdasda", footer="aaaaaaa")
+        self.add_group(wd, Group(groupname="Group1", header="asdasdasda", footer="aaaaaaa"))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
-    def add_empty_group(self):
+    def test_add_empty_group(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.add_group(wd, groupname="", header="", footer="")
+        self.add_group(wd, Group(groupname="", header="", footer=""))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
