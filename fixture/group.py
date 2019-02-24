fa293653f214a384bba6a5db5a51272dcfa71723
Anna Nedorya <a.nedorya@yandex.ru>
2019-02-24 16:50:43 +0300

    Выделение метода с проверкой if при заполнении формы в группе

--- fixture/group.py
+++ fixture/group.py
@@ -20,13 +20,15 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
 
-
-    def modify(self, group):
+    def modify_first_group(self, group):
         wd = self.app.wd
         self.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
+        # open modification form
         wd.find_element_by_name("edit").click()
         # edit group form
         self.fill_info(group)
@@ -35,18 +37,18 @@ class GroupHelper:
         self.return_to_groups_page()
 
     def fill_info(self, group):
-        wd = self.app.wd
         # edit group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.groupname)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.change_field_value("group_name", group.groupname)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
 
 
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def delete_first_group(self):
         wd = self.app.wd
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,11 +2,16 @@
 from model.group import Group
 
 
-def test_modify_group(app):
+def test_modify_group_name(app):
         app.session.login(username="admin", password="secret")
-        app.group.modify(Group(groupname="Group2", header="bbbbbbbb", footer="ccccccccc"))
+        app.group.modify_first_group(Group(groupname="Group2"))
         app.session.logout()
 
+
+def test_modify_group_header(app):
+        app.session.login(username="admin", password="secret")
+        app.group.modify_first_group(Group(header="bbbbbbbb"))
+        app.session.logout()
 
 
 
