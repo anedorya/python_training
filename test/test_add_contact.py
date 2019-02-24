db819de60c88e8bc1cf505ca308da41594e29719
Anna Nedorya <a.nedorya@yandex.ru>
2019-02-24 16:55:47 +0300

    Выделение login / logout в файл conftest.py

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,6 +3,4 @@ from model.contact import Info
 
 
 def test_add_contact(app):
-        app.session.login(username="admin", password="secret")
         app.contact.create(Info(firstname="qwerty", middlename="aaaaaaaa", lastname="eeeeeeeeee", nick="rrrrrrrrr", title="aaaaaaaaa", cname="ssssssssss", address="dddddddddddd", homedid="fffffffff", cellular="gggggggggg", workdid="hhhhhhhhh", fax="555555555", email="ppppppp", email2="ooooooo", email3="iiiiiiiiiiii", website="ffffffffffff", address2="ssssssssssssssssssssssssssssssssssssssssssss", home="555444333", notes="fffffffffffffffffffffffffffffffffffffffffff", byear="1980", ayear="2000"))
-        app.session.logout()
