b12c190d752d2125c28345d23ac38eadb19770e0
Anna Nedorya <a.nedorya@yandex.ru>
2019-02-24 15:22:10 +0300

    Заполнение групп и контактов выделено в отдельные методы.

--- fixture/group.py
+++ fixture/group.py
@@ -15,35 +15,12 @@ class GroupHelper:
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.groupname)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_info(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
-        def create(self, group):
-            wd = self.app.wd
-            self.open_groups_page()
-            # init group creation
-            wd.find_element_by_name("new").click()
-            # fill group form
-            wd.find_element_by_name("group_name").click()
-            wd.find_element_by_name("group_name").clear()
-            wd.find_element_by_name("group_name").send_keys(group.groupname)
-            wd.find_element_by_name("group_header").clear()
-            wd.find_element_by_name("group_header").send_keys(group.header)
-            wd.find_element_by_name("group_footer").click()
-            wd.find_element_by_name("group_footer").clear()
-            wd.find_element_by_name("group_footer").send_keys(group.footer)
-            # submit group creation
-            wd.find_element_by_name("submit").click()
-            self.return_to_groups_page()
+
 
     def modify(self, group):
         wd = self.app.wd
@@ -51,6 +28,14 @@ class GroupHelper:
         # select first group
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
+        # edit group form
+        self.fill_info(group)
+        # confirm changes
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+    def fill_info(self, group):
+        wd = self.app.wd
         # edit group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -60,9 +45,7 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # confirm changes
-        wd.find_element_by_name("update").click()
-        self.return_to_groups_page()
+
 
 
     def delete_first_group(self):
