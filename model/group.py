7a33b58951408bf2ed6ccb30d6fd6c771c0f49a2
Anna Nedorya <a.nedorya@yandex.ru>
2019-03-02 20:15:59 +0300

    Сравнение списка групп до/после при создании, редактировании, модицикации групп

--- model/group.py
+++ model/group.py
@@ -1,3 +1,4 @@
+from sys import maxsize
 
 class Group:
 
@@ -11,5 +12,10 @@ class Group:
         return "%s:%s" % (self.id, self.groupname)
 
     def __eq__(self, other):
-        return self.id == other.id and self.groupname == other.groupname
+        return (self.id is None or other.id is None or self.id == other.id) and self.groupname == other.groupname
 
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
