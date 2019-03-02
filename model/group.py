db8c155c9a2b502b56a7f9bff1ef3dbc60cc1405
Anna Nedorya <a.nedorya@yandex.ru>
2019-03-02 19:43:09 +0300

    Проверка равенства списка групп при удалении группы

--- model/group.py
+++ model/group.py
@@ -1,8 +1,15 @@
 
 class Group:
 
-    def __init__(self, groupname=None, header=None, footer=None):
+    def __init__(self, groupname=None, header=None, footer=None, id=None):
         self.groupname = groupname
         self.header = header
         self.footer = footer
+        self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.groupname)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.groupname == other.groupname
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,12 +5,16 @@ from model.group import Group
 def test_modify_group_name(app):
         if app.group.count() == 0:
                 app.group.create(Group(groupname="test"))
+        old_groups = app.group.get_group_list()
         app.group.modify_first_group(Group(groupname="Group2"))
+        new_groups = app.group.get_group_list()
+        assert len(old_groups) == len(new_groups)
 
 def test_modify_group_header(app):
         if app.group.count() == 0:
                 app.group.create(Group(groupname="test"))
+        old_groups = app.group.get_group_list()
         app.group.modify_first_group(Group(header="bbbbbbbb"))
-
-
+        new_groups = app.group.get_group_list()
+        assert len(old_groups) == len(new_groups)
 
