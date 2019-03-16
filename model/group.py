1bbf01538fd30c8c3695c32290dbb7078d7a63ab
Anna Nedorya <a.nedorya@yandex.ru>
2019-03-16 19:43:58 +0300

    Задание 15. Параметризация тестов для создания групп

--- model/group.py
+++ model/group.py
@@ -9,7 +9,7 @@ class Group:
         self.id = id
 
     def __repr__(self):
-        return "%s:%s" % (self.id, self.groupname)
+        return "%s:%s:%s:%s" % (self.id, self.groupname, self.header, self.footer)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.groupname == other.groupname
