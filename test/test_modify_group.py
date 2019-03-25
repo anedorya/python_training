565e2f52efcd6251c7bc87410df288a89858aa40
Anna Nedorya <a.nedorya@yandex.ru>
2019-03-25 09:20:50 +0300

    Задание 20-4. Исправление тестов на модификацию групп и контактов

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -19,12 +19,13 @@ def test_modify_group_name_db(app, db, check_ui):
                 app.group.create(Group(groupname="test"))
         old_groups = db.get_group_list()
         group = random.choice(old_groups)
-        new_group = Group(groupname="Group5")
-        app.group.modify_group_by_id(group.id, new_group)
+        changed_group = Group(groupname="Group2")
+        changed_group.id = group.id
+        app.group.modify_group_by_id(group.id, changed_group)
         assert len(old_groups) == app.group.count()
         new_groups = db.get_group_list()
         old_groups.remove(group)
-        old_groups.append(new_group)
+        old_groups.append(changed_group)
         assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
         if check_ui:
                 assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
